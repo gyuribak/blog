7fc4a90612c0200a7d79620438b27f3cfd5d8995
gyuribak <37536407+gyuribak@users.noreply.github.com>
2019-02-28 11:48:35 +0900

    blog update

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 import blogapp.views
 import portfolio.views
+import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',blogapp.views.blog, name = 'blog'),
     path('blogapp/', include('blogapp.urls')),
-   path('portfolio/', include('portfolio.urls')),
+    path('portfolio/', include('portfolio.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
