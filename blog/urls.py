cca9c3af8645916ba1ed31ae0e81f081769abc43
gyuribak <37536407+gyuribak@users.noreply.github.com>
2019-02-28 10:07:44 +0900

    update gyulblog

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import blogapp.views
+import portfolio.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/',blogapp.views.blog, name = 'blog'),
+    path('blogapp/', include('blogapp.urls')),
+   path('portfolio/', include('portfolio.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
