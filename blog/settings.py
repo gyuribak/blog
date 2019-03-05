8aab98087febc901e94c21e06b443ea8977bb927
gyuribak <37536407+gyuribak@users.noreply.github.com>
2019-03-05 22:48:27 +0900

    heroku commit

--- blog/settings.py
+++ blog/settings.py
@@ -8,11 +8,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7_am%nq9-&r00_99ej(j^$($+0&n5938v)qoe&d8vks0-f1hn2'
-
+#SECRET_KEY = '7_am%nq9-&r00_99ej(j^$($+0&n5938v)qoe&d8vks0-f1hn2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = []
 
 
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 # Create your views here.
 def blog(request):
     blogs = Blog.objects
-    return render(request,'blogs/blog.html',{'blogs' : blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request,'blogs/blog.html',{'blogs' : blogs, 'posts' :posts })
 
 def detail(request, blog_id):
     blogs_detail = get_object_or_404(Blog, pk=blog_id)
